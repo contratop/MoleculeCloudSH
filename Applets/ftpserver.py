b2f2f1b2b493943197e8204c998490a9130743a8
ContratopDev <contratop@molecule.net>
2020-11-26 18:41:47 +0100

    OpenFTP Updated 1.0 (Rewrite)

--- Applets/ftpserver.py
+++ Applets/ftpserver.py
@@ -4,31 +4,53 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 
-FTP_PORT = 8021
-FTP_DIRECTORY = os.getcwd()
+# The port the FTP server will listen on.
+# This must be greater than 1023 unless you run this script as root.
+FTP_PORT = 2121
+
+# The name of the FTP user that can log in.
+FTP_USER = "admin"
+
+# The FTP user's password.
+FTP_PASSWORD = "alpine"
+
+# The directory the FTP user will have full read/write access to.
+FTP_DIRECTORY = "/data/data/com.termux/files/home"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 IP = s.getsockname()[0]
 s.close()
 
-
 print(f'DIRECCION: ftp://{IP}:{FTP_PORT}')
 print(f'DIRECTORIO RAIZ: {FTP_DIRECTORY}')
+print(f'USUARIO: {FTP_USER}')
+print(f'CONTRASEÑA: {FTP_PASSWORD}')
 
 
-authorizer = DummyAuthorizer()
-authorizer.add_anonymous(FTP_DIRECTORY, perm='elradfmw')
+def main():
+    authorizer = DummyAuthorizer()
 
-handler = FTPHandler
-handler.authorizer = authorizer
-handler.banner = 'Molecule OpenFTP Server 0.9'
-handler.passive_ports = range(50000, 55535)
+    # Define a new user having full r/w permissions.
+    authorizer.add_user(FTP_USER, FTP_PASSWORD, FTP_DIRECTORY, perm='elradfmw')
 
-address = ('', FTP_PORT)
-server = FTPServer(address, handler)
+    handler = FTPHandler
+    handler.authorizer = authorizer
 
-server.max_cons = 256
-server.max_cons_per_ip = 5
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "pyftpdlib based ftpd ready."
 
-server.serve_forever()
+    # Optionally specify range of ports to use for passive connections.
+    #handler.passive_ports = range(60000, 65535)
+
+    address = ('', FTP_PORT)
+    server = FTPServer(address, handler)
+
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
